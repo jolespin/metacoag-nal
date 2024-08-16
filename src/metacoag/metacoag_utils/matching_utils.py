9137139ffa6b030212d43acd0c00a9747b2f3668
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2024-08-16 16:51:04 +0930

    REL: update version number

--- src/metacoag/metacoag_utils/matching_utils.py
+++ src/metacoag/metacoag_utils/matching_utils.py
@@ -11,7 +11,7 @@ from scipy.spatial import distance
 __author__ = "Vijini Mallawaarachchi and Yu Lin"
 __copyright__ = "Copyright 2020, MetaCoAG Project"
 __license__ = "GPL-3.0"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __maintainer__ = "Vijini Mallawaarachchi"
 __email__ = "vijini.mallawaarachchi@anu.edu.au"
 __status__ = "Stable Release"
